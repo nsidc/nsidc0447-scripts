54f392ade8505e42d36acaf4a2d1c3dc0b0fdd0b
Trey Stafford <trey.stafford@nsidc.org>
2021-03-30 16:16:03 -0600

    Script that produces output geotiff from ASCII

--- convert.py
+++ convert.py
@@ -3,10 +3,17 @@ from collections import defaultdict
 import re
 
 import numpy as np
+import rasterio as rio
+from rasterio.transform import Affine
 
 
 ascii_date_format = '%Y%m%d%H'
 ascii_date_regex = re.compile(r'\d{4}\d{2}\d{2}00')
+
+# These constants taken from the existing GeoTIFFs
+PROJ_WKT = 'PROJCS["unnamed",GEOGCS["unnamed ellipse",DATUM["unknown",SPHEROID["unnamed",6371200,0]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]]],PROJECTION["Polar_Stereographic"],PARAMETER["latitude_of_origin",60],PARAMETER["central_meridian",10],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1],AXIS["Easting",SOUTH],AXIS["Northing",SOUTH]]'
+DATASET_TRANSFORM = Affine(23812.498583569406, 0.0, -8405812.0, 0.0, -23812.498583569406, 8405812.0)
+DATA_DTYPE = np.float64
 
 
 def read_daily_ascii_file(input_filepath):
@@ -16,7 +23,7 @@ def read_daily_ascii_file(input_filepath):
     `data` is a numpy array.
 
     """
-    data = defaultdict(list)
+    str_datas = defaultdict(list)
     with open(input_filepath, 'r') as f:
         for line in f:
             line = line.strip()
@@ -24,15 +31,33 @@ def read_daily_ascii_file(input_filepath):
                 date = dt.datetime.strptime(line, ascii_date_format).date()
                 continue
 
-            data[date].append(line.split())
+            str_datas[date].append(line.split())
+
+    data = {}
+    for date, str_data in str_datas.items():
+        data[date] = np.array(str_data, dtype=DATA_DTYPE)
+
+    return data
 
 
-    for date, str_data in data.items():
-        yield date, np.array(str_data, dtype=np.float64)
+def make_geotiff(input_ascii_fp, output_geotiff_fp):
+    data = read_daily_ascii_file(input_ascii_fp)
+    nbands = len(data.keys())
+
+    crs = rio.crs.CRS().from_wkt(PROJ_WKT)
+    with rio.open(output_geotiff_fp, 'w',
+            driver='GTiff',
+            count=nbands,
+            height=706,
+            width=706,
+            dtype=DATA_DTYPE,
+            crs=crs,
+            transform=DATASET_TRANSFORM) as out_ds:
+        for idx, (date, np_array) in enumerate(data.items(), start=1):
+            print(f'Writing {date} to band {idx}')
+            out_ds.write(np_array, idx)
 
 
 if __name__ == '__main__':
     input_fp = '/home/trst2284/code/nsidc0447-geotiffs/localdata/NSIDC-0447-2020/cmc_daily_analysis_2020.txt'
-    for date, data in read_daily_ascii_file(input_fp):
-        breakpoint()
-        pass
+    make_geotiff(input_fp, '/tmp/test.tif')
