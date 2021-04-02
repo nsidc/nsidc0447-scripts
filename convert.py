d0a2c66245a4ca5bb2293955b87c99bed8e2b2ee
Trey Stafford <trey.stafford@nsidc.org>
2021-04-02 16:32:26 -0600

    Add simple test against existing data.

--- convert.py
+++ convert.py
@@ -69,6 +69,38 @@ def make_geotiff(input_ascii_fp, output_geotiff_fp):
     gdal.Info(str(output_geotiff_fp), stats=True)
 
 
+def convert_2020():
+    input_fp = 'localdata/NSIDC-0447-2020/cmc_daily_analysis_2020.txt'
+    output_dir = Path('localdata/convert_outputs_2021/Snow_Depth/Snow_Depth_Daily_Values/GeoTIFF/')
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Make the output filename consistent with the existing filenaming
+    # structure.
+    output_fp = output_dir / 'cmc_sdepth_dly_2020_v01.2.tif'
+    make_geotiff(input_fp, output_fp)
+
+
+def crosscheck_existing():
+    input_fp = 'localdata/test/cmc_sdepth_dly_2001_v01.2.txt'
+    output_dir = Path('localdata/test/cmc_sdepth_dly_2001_v01.2.tif')
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    output_fp = output_dir / 'cmc_sdepth_dly_2020_v01.2.tif'
+    make_geotiff(input_fp, output_fp)
+
+    new_ds = rio.open(output_fp, 'r')
+    old_ds = rio.open('localdata/projects/DATASETS/nsidc0447_CMC_snow_depth_v01/Snow_Depth/Snow_Depth_Daily_Values/GeoTIFF/cmc_sdepth_dly_2001_v01.2.tif', 'r')
+
+    assert new_ds.count == old_ds.count
+    for band_idx in range(1, new_ds.count + 1):
+        new_data = new_ds.read(band_idx)
+        old_data = old_ds.read(band_idx)
+        print(f'checking {band_idx}')
+        if not np.all(new_data == old_data):
+            breakpoint()
+    print('Done!')
+
+
 if __name__ == '__main__':
-    input_fp = '/home/trst2284/code/nsidc0447-geotiffs/localdata/NSIDC-0447-2020/cmc_daily_analysis_2020.txt'
-    make_geotiff(input_fp, '/tmp/test.tif')
+    convert_2020()
+    # crosscheck_existing()
